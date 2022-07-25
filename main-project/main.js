f2f1e53fecee77005e84e8462e13172093d7f0d4
Tarik Souabny <75922626+tariksouabny@users.noreply.github.com>
2022-07-25 14:53:40 +0000

    Add red outline on all divs (debug)

--- main-project/main.js
+++ main-project/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+// debug - red outline on all divs
+
+const allDivs = document.querySelectorAll('*');
+
+allDivs.forEach(
+    (el) => {
+        el.style.outline = "1px solid red";
+    }
+)
